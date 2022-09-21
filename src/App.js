f6cc9b68af341ea7fe7fabbf320d49aeda8eaaa6
alexcesi <alexvesier@gmail.com>
2022-09-21 15:37:37 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-const App = () =>
-  <h1>Hello, World!</h1>
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          La CI/CD marche a chaque commit !
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-export default App
+export default App;
